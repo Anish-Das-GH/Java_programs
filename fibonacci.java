b25e86c0053e0ae4876f465f8e672eb4ffa393be
Anish Das <anishservice12@gmail.com>
2023-07-22 13:48:30 +0530

    v 1.3

--- fibonacci.java
+++ fibonacci.java
@@ -1,3 +1,34 @@
-public class fibonacci {
-    
+import java.util.*;
+class func{
+    void fibonacci()
+    {
+        int a,b,c,i;
+        a = 0;
+        b = 1;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter value of n");
+        int n = sc.nextInt();
+        System.out.println("Fibonacci Series : ");
+        System.out.println(a);
+        System.out.println(b);
+        for(i=0;i<=n;i++)
+        {
+            c=a+b;
+            System.out.println(c);
+            a=b;
+            b=c;
+
+
+
+        }
+
+    }
+}
+
+class fibonacci{
+    public static void main(String[] args)
+    {
+        func f = new func();
+        f.fibonacci();
+    }
 }
